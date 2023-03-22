fcc01cb465a26d85bf56568d671194f076b8d840
Neriya <neriyashul@gmail.com>
2023-03-22 16:37:10 +0200

    style: style 404 page

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,6 +1,14 @@
-import React from "react"
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import styles from "./style";
 
 export default function NotFound() {
-    return <p>404 - דף לא נמצא</p>
+    return (
+        <Box sx={styles.container}>
+            <Typography component="h1" sx={styles.header}>
+                404 - דף לא נמצא
+            </Typography>
+        </Box>
+    );
 }
-  
---
+++ src/pages/404/style.js
@@ -0,0 +1,13 @@
+const styles = {
+    container: {
+        height: "75vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    header: {
+        fontSize: { xs: "2rem", md: "3rem" },
+    },
+};
+export default styles;
