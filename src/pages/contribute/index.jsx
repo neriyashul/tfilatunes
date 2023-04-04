416811b9561de44b2feab1a3090ab660bde649d1
Neriya <neriyashul@gmail.com>
2023-04-05 00:58:11 +0300

    feat: add contribute page

---
+++ src/pages/contribute/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Box, Link as MuiLink, Typography } from "@mui/material";
+import { styles } from "./style";
+import { Link } from "react-router-dom";
+
+export default function Contribute() {
+    return (
+        <Box sx={styles.container}>
+            <Typography variant="h4" component="h1" sx={styles.header}>
+                איך אפשר לעזור?
+            </Typography>
+            <ul>
+                <Box component="li" sx={styles.listItem}>
+                    יש לך רעיון למנגינה שלא מופיעה באתר? ניתן להוסיף אותה&nbsp;
+                    <MuiLink component={Link} to="/add-tune">
+                        כאן
+                    </MuiLink>
+                </Box>
+                <Box component="li" sx={styles.listItem}>
+                    {"יש לך שאלה, רעיון או בקשה? מוזמן/ת לפנות אלינו דרך המייל: "}
+                    <MuiLink href="mailto: support@tfilatunes.com">
+                        support@tfilatunes.com
+                    </MuiLink>
+                </Box>
+                <Box component="li" sx={styles.listItem}>
+                    רוצה להשתתף בפיתוח האתר או בעיצוב שלו? האתר הוא קוד פתוח
+                    ונמצא&nbsp; ב
+                    <MuiLink href="https://github.com/neriyashul/tfilatunes">
+                        גיטהאב
+                    </MuiLink>
+                </Box>
+            </ul>
+        </Box>
+    );
+}
---
+++ src/pages/contribute/style.js
@@ -0,0 +1,13 @@
+export const styles = {
+    container: {
+        marginX: "4%",
+        marginY: 5,
+    },
+
+    header: { my: 4, mx: 3 },
+
+    listItem: {
+        fontSize: "1.3rem",
+        margin: "17px 0",
+    },
+};
