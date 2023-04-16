e7df6178d6c2da72b77e04b9ef386e03c8811520
Neriya <neriyashul@gmail.com>
2023-04-16 23:50:34 +0300

    fix: disable query if param is invalid

--- src/hooks/tunes.js
+++ src/hooks/tunes.js
@@ -5,17 +5,19 @@ const db_type = import.meta.env.VITE_DB_TYPE;
 const db = getDB(db_type);
 
 export function useTunes(subsectionId) {
-    const { isLoading, data, error } = useQuery(`tunes-${subsectionId}`, () =>
-        Promise.resolve(db.getTunes(subsectionId))
+    const { isLoading, data, error } = useQuery(
+        `tunes-${subsectionId}`,
+        () => Promise.resolve(db.getTunes(subsectionId)),
+        { enabled: Boolean(subsectionId) }
     );
     return { isLoading, tunes: data, error };
 }
 
 export function useTune(id, subsectionId) {
-    if (!id) return {};
     const { isLoading, data, error } = useQuery(
         `tune-${id}-sub-${subsectionId}`,
-        () => Promise.resolve(db.getTune(id, subsectionId))
+        () => Promise.resolve(db.getTune(id, subsectionId)),
+        { enabled: Boolean(id) && Boolean(subsectionId) }
     );
     return { isLoading, tune: data, error };
 }
