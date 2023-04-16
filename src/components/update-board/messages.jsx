5f8ab191a5d08cb2d2fe84add219491e3133bba6
Neriya <neriyashul@gmail.com>
2023-04-16 20:43:21 +0300

    style: add update message about yom-haatzmaut

--- src/components/update-board/messages.jsx
+++ src/components/update-board/messages.jsx
@@ -1,4 +1,22 @@
 import { Box } from "@mui/material";
 import styles from "./style";
 
-export default [];
+export default [
+    {
+        id: "tfila-yom-haatzmaut",
+        getMessage: () => (
+            <>
+                תפילות ל
+                <Box
+                    sx={styles.link}
+                    component="a"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="/collection/yom-haatzmaut"
+                >
+                    יום&nbsp;העצמאות
+                </Box>
+            </>
+        ),
+    },
+];
