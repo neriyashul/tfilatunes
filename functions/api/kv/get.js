0b26e154af94fd58cea47e857f1549cdbfbf755b
Neriya <neriyashul@gmail.com>
2023-04-18 20:31:09 +0300

    fix: add await

--- functions/api/kv/get.js
+++ functions/api/kv/get.js
@@ -4,7 +4,7 @@ export async function onRequestGet({ env }) {
     const cache = new Cache(env.CLOUDFLARE_KV);
 
     try {
-        const value = cache.get("dummy");
+        const value = await cache.get("dummy");
         return new Response("SUCCESS: " + value);
     } catch (error) {
         return new Response(`FAILED: ${error}`);
--- functions/api/kv/set.js
+++ functions/api/kv/set.js
@@ -5,7 +5,7 @@ export async function onRequestGet({ env }) {
 
     try {
         const epochIn24Hours = Math.round(Date.now() / 1000) + 86000;
-        cache.set("dummy", `The time is: ${Math.round(Date.now() / 1000)}`, epochIn24Hours);
+        await cache.set("dummy", `The time is: ${Math.round(Date.now() / 1000)}`, epochIn24Hours);
         return new Response("SUCCESS")
     } catch (error) {
         return new Response(`FAILED: ${error}`)
