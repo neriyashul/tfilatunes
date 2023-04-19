a89b73af498f9502997272d6c14856d07d1036b3
Neriya <neriyashul@gmail.com>
2023-04-20 00:40:29 +0300

    fix: add import to Cache

--- functions/files/tunes/[filename].pdf.js
+++ functions/files/tunes/[filename].pdf.js
@@ -1,6 +1,7 @@
 import { Base64 } from "js-base64";
 import { InvalidParameterResponse } from "../../utils/response";
 import { generateHtml } from "./generate-html";
+import { Cache } from "../../utils/cache";
 
 async function streamToString(stream) {
     const strings = [];
