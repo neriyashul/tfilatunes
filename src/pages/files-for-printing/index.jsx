60311851c45d8e0add59feec8567fcd538e2bbac
Neriya <neriyashul@gmail.com>
2023-04-21 04:27:57 +0300

    feat: change collection to accordion in /files

--- src/pages/files-for-printing/index.jsx
+++ src/pages/files-for-printing/index.jsx
@@ -1,61 +1,203 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
-import { Box, Button, Typography } from "@mui/material";
+import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+    Backdrop,
+    Box,
+    Button,
+    CircularProgress,
+    Typography,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import styles from "./style";
 import tfilot from "../../db/data/tfilot.json";
 
+const messages = [
+    "מייצר קובץ עם כל השירים...",
+    "ממיר את הקובץ ל-html...",
+    "מסיים...",
+];
+
 export default function FilesForPrinting() {
-    const [value, setValue] = React.useState("קבלת_שבת");
+    const [selected, setSelected] = React.useState("קבלת_שבת");
+    const [msgIndex, setMsgIndex] = React.useState(0);
+    const [currentCollection, setCurrentCollection] = React.useState();
+    const [isProgressOpen, setIsProgressOpen] = React.useState(false);
 
     const handleChange = (event) => {
-        setValue(event.target.value);
+        setSelected(event.target.value);
     };
+
+    useEffect(() => {
+        let intervId;
+        let currentIndex = msgIndex;
+
+        const stopInterval = () => {
+            if (intervId) clearInterval(intervId);
+            intervId = null;
+        };
+
+        if (!intervId && isProgressOpen) {
+            intervId = setInterval(() => {
+                if (currentIndex < messages.length - 1) {
+                    setMsgIndex((prev) => prev + 1);
+                    currentIndex++;
+                } else {
+                    stopInterval();
+                }
+            }, 2750);
+        }
+
+        return () => stopInterval();
+    }, [isProgressOpen]);
+
+    const elements = [];
+    const collections = {};
+
+    for (let tfila of tfilot) {
+        if (tfila.collection) {
+            let collection = collections[tfila.collection] || [];
+            collection.push(tfila);
+            collections[tfila.collection] = collection;
+        } else {
+            elements.push(tfila);
+        }
+    }
 
     return (
         <Box sx={styles.center}>
-            <Box component="form" sx={styles.form}>
+            <Box sx={styles.form}>
                 <Typography component="h1" sx={styles.header}>
                     קבצי&nbsp;מנגינות&nbsp;להדפסה
                 </Typography>
-                <FormControl>
-                    <FormLabel
-                        id="radio-buttons-lablel"
-                        sx={{ fontSize: "1.5rem" }}
-                    >
-                        בחר תפילה
-                    </FormLabel>
-                    <RadioGroup
-                        aria-labelledby="radio-buttons-lablel"
-                        name="controlled-radio-buttons-group"
-                        value={value}
-                        onChange={handleChange}
-                    >
-                        {tfilot.map((tfila, index) => (
-                            <FormControlLabel
-                                key={index}
-                                value={tfila.name.split(" ").join("_")}
-                                control={<Radio />}
-                                label={tfila.name}
-                            />
-                        ))}
-                    </RadioGroup>
-                </FormControl>
-                <Box
-                    component="a"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href={`/files/tunes/מנגינות_ל${value}.pdf`}
-                    sx={styles.link}
-                >
-                    <Button variant="contained" sx={styles.button}>
-                        הורד קובץ
-                    </Button>
-                </Box>
+                <form>
+                    <FormControl>
+                        <FormLabel id="radio-buttons-lablel" sx={styles.label}>
+                            בחר תפילה
+                        </FormLabel>
+
+                        <RadioGroup
+                            aria-labelledby="radio-buttons-lablel"
+                            name="controlled-radio-buttons-group"
+                            value={selected}
+                            onChange={handleChange}
+                        >
+                            {elements.map((tfila, index) => (
+                                <FormControlLabel
+                                    required
+                                    key={index}
+                                    value={tfila.name.split(" ").join("_")}
+                                    control={<Radio />}
+                                    label={tfila.name}
+                                />
+                            ))}
+                            {Object.entries(collections).map(
+                                ([collectName, tfilot]) => {
+                                    const isOpen =
+                                        currentCollection === collectName;
+                                    return (
+                                        <FormControlLabel
+                                            key={collectName}
+                                            control={
+                                                <Radio
+                                                    onChange={() =>
+                                                        setCurrentCollection(
+                                                            (prev) =>
+                                                                prev !==
+                                                                collectName
+                                                                    ? collectName
+                                                                    : null
+                                                        )
+                                                    }
+                                                    sx={styles.collectionRadio(
+                                                        isOpen
+                                                    )}
+                                                />
+                                            }
+                                            label={
+                                                <Accordion
+                                                    expanded={isOpen}
+                                                    elevation={0}
+                                                    sx={styles.accordion}
+                                                >
+                                                    <AccordionSummary
+                                                        expandIcon={
+                                                            <ExpandMoreIcon />
+                                                        }
+                                                        aria-controls="panel1a-content"
+                                                        id="panel1a-header"
+                                                    >
+                                                        <Typography>
+                                                            {collectName}
+                                                        </Typography>
+                                                    </AccordionSummary>
+                                                    <AccordionDetails>
+                                                        {tfilot.map(
+                                                            (tfila, index) => (
+                                                                <FormControlLabel
+                                                                    key={index}
+                                                                    value={tfila.name
+                                                                        .split(
+                                                                            " "
+                                                                        )
+                                                                        .join(
+                                                                            "_"
+                                                                        )}
+                                                                    control={
+                                                                        <Radio />
+                                                                    }
+                                                                    label={
+                                                                        tfila.name
+                                                                    }
+                                                                />
+                                                            )
+                                                        )}
+                                                    </AccordionDetails>
+                                                </Accordion>
+                                            }
+                                        ></FormControlLabel>
+                                    );
+                                }
+                            )}
+                        </RadioGroup>
+                    </FormControl>
+
+                    <Box sx={styles.buttonContainer}>
+                        <Button
+                            variant="contained"
+                            sx={styles.button}
+                            onClick={() => {
+                                if (selected) {
+                                    setMsgIndex(0);
+                                    setIsProgressOpen(true);
+                                    window.location.href = `/files/tunes/מנגינות_ל${selected}.pdf`;
+                                } else {
+                                    alert("לא נבחר אף קובץ");
+                                }
+                            }}
+                        >
+                            הורד קובץ
+                        </Button>
+                    </Box>
+                </form>
             </Box>
+            <Backdrop
+                open={isProgressOpen}
+                onClick={() => setIsProgressOpen(false)}
+            >
+                <Box sx={styles.backdropContainer}>
+                    <CircularProgress color="inherit" />
+                    <Typography component="h2" variant="h5">
+                        {messages[msgIndex]}
+                    </Typography>
+                </Box>
+            </Backdrop>
         </Box>
     );
 }
--- src/pages/files-for-printing/style.js
+++ src/pages/files-for-printing/style.js
@@ -1,7 +1,7 @@
 const styles = {
     form: {
         m: 5,
-        width: "min-content"
+        width: "min-content",
     },
 
     header: {
@@ -10,23 +10,39 @@ const styles = {
         mb: 4,
     },
 
+    label: {
+        fontSize: "1.5rem",
+    },
+
     center: {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
     },
 
-    link: {
-        textDecoration: "none",
+    accordion: {
+        backgroundColor: "#121212",
+        ml: -2,
+    },
+
+    buttonContainer: {
         display: "flex",
         justifyContent: "center",
-        alignItems: "center",
     },
 
     button: {
         mt: 5,
         width: 150,
         height: 45,
+    },
+
+    collectionRadio: (isOpen) => ({
+        zIndex: 10,
+        visibility: isOpen ? "hidden" : "visible",
+    }),
+
+    backdropContainer: {
+        textAlign: "center",
     },
 };
 
