9f73e0312970b9e67e1aafc91ac542198b31709f
neriya shulman <45920392+neriyashul@users.noreply.github.com>
2023-11-02 23:38:24 +0200

    Remove old changes in messages.jsx

--- src/components/update-board/messages.jsx
+++ src/components/update-board/messages.jsx
@@ -2,55 +2,20 @@ import { Box } from "@mui/material";
 import styles from "./style";
 
 export default [
-    {
-        id: "tfila-yom-haatzmaut",
-        getMessage: (isMobile) => (
-            <>
-                <Box
-                    sx={styles.link}
-                    component="a"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="/files"
-                >
-                    קבצי מנגינות להדפסה
-                </Box>
-
-                <br />
-                {isMobile ? (
-                    <small>
-                        כדי לנווט לעמוד ההורדה:
-                        <ol>
-                            <li>לחיצה על התפריט מצד שמאל למעלה</li>
-                            <li>&quot;קבצים&nbsp;להדפסה&quot;</li>
-                        </ol>
-                        <br />
-                    </small>
-                ) : (
-                    <small>
-                        ניתן להוריד את הקבצים דרך &quot;קבצים להדפסה&quot;
-                        בתפריט בראש&nbsp;העמוד
-                    </small>
-                )}
-            </>
-        ),
-    },
-    {
-        id: "tfila-yom-yerushalayim",
-        getMessage: () => (
-            <>
-                תפילות ל
-                <Box
-                    sx={styles.link}
-                    component="a"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="/collection/yom-yerushalayim"
-                >
-                    יום&nbsp;ירושלים
-                </Box>
-            </>
-        ),
-    },
-    
+    // {
+    //     id: "tfila-yom-yerushalayim",
+    //     getMessage: () => (
+    //         <>
+    //             תפילות ל
+    //             <Box
+    //                 sx={styles.link}
+    //                 component="a"
+    //                 target="_blank"
+    //                 rel="noopener noreferrer"
+    //                 href="/collection/yom-yerushalayim"
+    //             >
+    //                 יום&nbsp;ירושלים
+    //             </Box>
+    //         </>
+    //     ),
 ];
