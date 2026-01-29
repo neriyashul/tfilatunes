a2f8e5d13f59e0d83b83c2f083c6efc5f481fb80
Neriya Shulman <neriyashu@Neriyas-MacBook-Pro.local>
2026-01-29 20:51:06 +0200

    fix: comment serverDB

--- src/db/db-factory.js
+++ src/db/db-factory.js
@@ -2,12 +2,13 @@ import DBGateway from "./db-gateway";
 import { FileSystemDB } from "./file-system-db";
 
 export default function getDB(db_type) {
-    switch (db_type) {
-        case "file_system":
-            return new FileSystemDB();
-        case "server":
-            return new DBGateway();
-        default:
-            throw new TypeError(`${db_type} is not a valid database`);
-    }
+    // switch (db_type) {
+    //     case "file_system":
+    //         return new FileSystemDB();
+    //     case "server":
+    //         return new DBGateway();
+    //     default:
+    //         throw new TypeError(`${db_type} is not a valid database`);
+    // }
+    return new FileSystemDB();
 }
